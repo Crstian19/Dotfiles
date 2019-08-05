634f7ed13fe1424f820cd5fb81c3747d06c9ff42
Cristian <kiyoma19@gmail.com>
2019-08-05 23:26:59 +0200

    Change ping script color

--- .config/polybar/scripts/info-pingrtt.sh
+++ .config/polybar/scripts/info-pingrtt.sh
@@ -8,7 +8,7 @@ else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
     if [ "$rtt" -lt 50 ]; then
-        icon=""
+        icon="%{F#3cb703}%{F-}"
     elif [ "$rtt" -lt 150 ]; then
         icon="%{F#f9dd04}%{F-}"
     else
